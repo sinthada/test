e867bdd3a05cc45918e0e27682af5dffe3e666ae
sinthada <81430792+sinthada@users.noreply.github.com>
2024-11-10 14:19:54 +0700

    Update app_predict_penguin_XXX.py

--- app_predict_penguin_XXX.py
+++ app_predict_penguin_XXX.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 %matplotlib inline
 import seaborn as sns
 import pickle
